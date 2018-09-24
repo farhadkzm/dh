a0b8e25bebc8a13c13eec190f1bdb29004e50513
Farhad Kazemi <farhadkazemi@nbnco.com.au>
2018-09-24 16:31:41 +1000

    added connection to es

--- ui/app/app.js
+++ ui/app/app.js
@@ -1,10 +1,14 @@
 // content of index.js
 const http = require('http')
+const request = require('request');
+
+const url = 'http//dh-stack-elasticsearch-client:9200/log/_search'
 const port = 3000
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  response.end('Hello Node.js Server!')
+  var es_response = await axios.get(url);
+  response.end('Hello Node.js Server!. ES response: ' + es_response)
 }
 
 const server = http.createServer(requestHandler)
