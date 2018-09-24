5764b7750f11c3b17489be36c84d6392b0e93a62
Farhad Kazemi <farhadkazemi@nbnco.com.au>
2018-09-24 16:47:37 +1000

    fixed issue with es url

--- ui/app/app.js
+++ ui/app/app.js
@@ -1,13 +1,29 @@
 // content of index.js
 const http = require('http')
-const request = require('request');
+const request = require('axios');
 
-const url = 'http//dh-stack-elasticsearch-client:9200/log/_search'
+const elastic_url = 'http//dh-stack-elasticsearch-client:9200/log/_search'
 const port = 3000
+
+const getLocation = async url => {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    console.log(
+      `City: ${data.results[0].formatted_address} -`,
+      `Latitude: ${data.results[0].geometry.location.lat} -`,
+      `Longitude: ${data.results[0].geometry.location.lng}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  var es_response = await axios.get(url);
+  var es_response = getLocation(elastic_url);
   response.end('Hello Node.js Server!. ES response: ' + es_response)
 }
 
