3ae0204fde762085a8184cc87765506a8ccdf2da
Farhad Kazemi <fk231n@att.com>
2018-10-02 15:06:11 +1000

    added backup for app script

--- ui/app/app.js
+++ ui/app/app.js
@@ -5,36 +5,106 @@ const fetch = require('node-fetch')
 
 const app = express()
 //const elastic_url ='http//dh-stack-elasticsearch-client:9200/log/_search'
-const elastic_url ='http://localhost:9200/log/_search'
+const elastic_url ='http://localhost:9200'
+
+const execution_url =`${elastic_url}/log/_search`
 
 const port = process.env.PORT || 3000
 
-function get(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then(res1 => res1.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err))
+function hasEndMessageReceived(data){
+  var body = {
+    "query": {
+      "bool" : {
+        "must" : {
+          "term" : { "execution_id" : data.id },
+          "match" : {  "message" : "finished"}
+        }
+      }
+    }
+  }
+  return fetch(execution_url, {
+    method: 'POST',
+    body:    JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
   })
+  .then(res1 => res1.json())
+  .then(res1 => { hasEndMessageReceived:res1, data})
+
 }
 
+function getExecutions(){
 
-app.get('/', (request, response) => {
-  Promise.all([
-    get(elastic_url)
-  ]).then(([es_res]) =>
+  var body = {
+    "size": 0,
+    "aggs" : {
+      "group_by_execution_id" : {
+
+        "terms": {
+          "field": "execution_id"
+        },
+        "aggs" : {
+          "execution_max" : { "max" : { "field" : "created" } },
+          "execution_min" : { "min" : { "field" : "created" } }
+        }
+
+      }
+    }
+  }
+  //select all executions that are in-progress
+  //handle timeout
+  //handle status
+
+  return new Promise((resolve, reject) => {
+
+
+    var resArray = fetch(execution_url, {
+      method: 'POST',
+      body:    JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    .then(res1 => res1.json())
+    .then(data =>
+      {
+
+        return data.aggregations.group_by_execution_id.buckets
+        .map(exec=> {
+          return {id: exec.key,
+            last_log: exec.execution_max.value,
+            first_log: exec.execution_min.value}
+          });
+        })
+    .then(data => {
+          return data.map(exec=> {return hasEndMessageReceived(exec);})
+        });
+
+    resolve( Promise.all(resArray).then(function(values) {
+          console.log(values)
+        }))
+
+      })
+      .catch(err => reject(err))
+
+  }
+  //num of txn over a time period
+  //run agg req to get list of execution periods
+
+  app.get('/', (request, response) => {
+    Promise.all([
+      //get(log_url)
+      getExecutions()
+    ]).then(([es_res]) =>
     response.send({
       query: es_res
     }))
     .catch(err => {
       console.log (err)
       response.send('Ops, something has gone wrong', er)})
-})
+    })
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+    app.listen(port, (err) => {
+      if (err) {
+        return console.log('something bad happened', err)
+      }
 
-  console.log(`server is listening on ${port}`)
-})
+      console.log(`server is listening on ${port}`)
+    })
