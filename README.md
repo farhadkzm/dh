15e2872b2487ff4b496efd8c5e812261ffae1b61
Farhad Kazemi <farhadkazemi@nbnco.com.au>
2018-10-08 23:50:27 +1100

    added proxy and template

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-After creating a project on Google Cloud and installing gcloud, run the following commands to enable Deployment Manager on the project.
- (Replace `dh-v1-216802` with the actual project id)
+# Setting up the cluster
+
+Checkout this repository on your local machine.
+
+Create a Cloud Project on Google Cloud, install gcloud cli on your machine, and then run the following commands to enable Deployment Manager on the project.
+ (Replace `dh-v1-216802` with your project id)
 ```sh
 export PROJECT='dh-v1-216802'
 gcloud services enable deploymentmanager.googleapis.com --project=$PROJECT
 ```
 
-Checkout this git repository and run the Google Deployment Manager script
+Set up a Kubernetes cluster using Google Deployment Manager (Change NUM_NODES to create a larger cluster with more resources):
 
 ```sh
 export CLUSTER_NAME='dh-mini'
@@ -34,3 +38,28 @@ helm install --name dh-stack digital-stack/
 ```
 
 
+# Setting up the UI application
+
+To set up and run the ui application on your local machine you need to have access to the Digital Hub cluster.
+
+Download **gcloud cli** and **kubectl** and place them in your system PATH.
+
+Kubectl cli needs credentials to connect to a remote cluster. You need to use gcloud cli to prepare the environment and set the right environment variable (KUBECONFIG) to be used by kubectl.
+
+The following command places the required credentials (Make sure project id is the right one):
+
+```sh
+gcloud container clusters get-credentials dh-mini --zone us-west1-a --project dh-v1-216802
+```
+
+Now we need to do some port-forwarding magics to make Elasticsearch and Kibana available on your local machine:
+
+```sh
+# Kibana port forwarding on port 5601
+kubectl port-forward dh-stack-kibana-97464f66d-p6jml 5601:5601  
+
+# Elasticsearch port forwarding on port 9200
+kubectl port-forward dh-stack-elasticsearch-client-868d6f98fd-nz8rg 9200:9200
+```
+
+Install the UI app dependencies by running **npm install** in ui/app/ and run the app using **node ui/app/app.js**
--- ui/app/app.js
+++ ui/app/app.js
@@ -1,139 +1,36 @@
-// content of index.js
+
 const http = require('http')
 const express = require('express')
-const fetch = require('node-fetch')
-
+var exphbs = require('express-handlebars');
 const app = express()
-//const elastic_url ='http//dh-stack-elasticsearch-client:9200/log/_search'
-const elastic_url ='http://localhost:9200'
 
 const port = process.env.PORT || 3000
-const FINISHED = "finished"
+const chart_url = 'http://localhost:5601/app/kibana#/visualize/edit/16cd8760-cac0-11e8-9e8c-137d8010fd52?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-5y%2Cmode%3Aquick%2Cto%3Anow))'
 
-//get the list of executions with min,max log datetime
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-//calculate period for each in minutes
+app.get('/chart_iframe', function (req, res) {
+  res.render('home', { chart1_url: '/chart_proxy' });
+});
+app.get('/chart_proxy', function (req, res) {
 
-//calculate status of each execution
+  req.pipe(require('request')(chart_url)).pipe(res);
 
-//calculate licence utilization
-//calculate number of sessions for each process within a given time
+  // res.render('home',{chart1_url: chart_url});
+});
+app.use(function (req, res) {
 
+  console.log('originalUrl', req.originalUrl)
+  console.log('rewriting to ', 'http://localhost:5601' + req.originalUrl)
+  req.pipe(require('request')('http://localhost:5601' + req.originalUrl)).pipe(res);
 
-
-function enhanceForStatus(data){
-  var body = {
-    "query": {
-      "bool" : {
-        "must" : [
-          {"term" : { "execution_id" : data.execution_id }},
-          {"match" : {  "message" : FINISHED}}
-        ]
-      }
-    }
+  // res.render('home',{chart1_url: chart_url});
+});
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
   }
 
-  return fetch(`${elastic_url}/log/_count`, {
-    method: 'POST',
-    body:    JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
-  })
-  .then(res1 => res1.json())
-  .then(res1 => {
-    return {hasFinished:res1.count, ...data}
-  });
-
-}
-
-function processStatus(data){
-
-  var status = "FINISHED"
-
-  const timoutPeriod = 1000*60*20 //20 minutes
-  if(data.hasFinished != 0){
-    if (data.last_log + timoutPeriod < new Date().getTime())
-    {  status = "TIME_OUT"}
-    else {
-      status = "IN_PROGRESS"
-    }
-
-  }
-
-  return {...data, status}
-}
-
-function getExecutions(){
-
-  var body = {
-    "size": 0,
-    "aggs" : {
-      "group_by_execution_id" : {
-
-        "terms": {
-          "field": "execution_id"
-        },
-        "aggs" : {
-          "execution_max" : { "max" : { "field" : "created" } },
-          "execution_min" : { "min" : { "field" : "created" } }
-        }
-
-      }
-    }
-  }
-
-  return new Promise((resolve, reject) => {
-
-
-    var resArray = fetch(`${elastic_url}/log/_search`, {
-      method: 'POST',
-      body:    JSON.stringify(body),
-      headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res1 => res1.json())
-
-    .then(data =>{
-      return data.aggregations.group_by_execution_id.buckets
-      .map(bucket=> {
-        return {execution_id: bucket.key,
-          last_log: bucket.execution_max.value,
-          first_log: bucket.execution_min.value}
-        });
-      })
-      .then(data =>{
-        return data.map(exec=> {
-          var difference = exec.last_log - exec.first_log
-          const days = 1000*60*60.0
-          var period_in_hours = Math.floor(difference/days);
-          return {...exec, period_in_hours}
-        } )
-      })
-      .then(data => Promise.all(data.map(item => enhanceForStatus(item))))
-      .then(data => data.map(item => processStatus(item)))
-      .then(data => {resolve(data)})
-      .catch(err => reject(err))
-
-    });
-  }
-  //num of txn over a time period
-  //run agg req to get list of execution periods
-
-  app.get('/', (request, response) => {
-    Promise.all([
-      //get(log_url)
-      getExecutions()
-    ]).then(([es_res]) =>
-    response.send({
-      query: es_res
-    }))
-    .catch(err => {
-      console.log (err)
-      response.send('Ops, something has gone wrong', er)})
-    })
-
-    app.listen(port, (err) => {
-      if (err) {
-        return console.log('something bad happened', err)
-      }
-
-      console.log(`server is listening on ${port}`)
-    })
+  console.log(`server is listening on ${port}`)
+})
